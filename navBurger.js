8fcf44ff02fa159ba1959b7b7d063ff0f5ea6da6
charlesQuigley <37884135+charlesQuigley@users.noreply.github.com>
2022-02-17 16:15:49 -0600

    phone burger icon and menu done

--- navBurger.js
+++ navBurger.js
@@ -1,3 +1,15 @@
+
+//Array that holds all elements whose visibility needs to be hidden
+//when the phone menu is displayed.
+//All elements that use intersection observers disregard z-index for some reason,
+//so, these elements will be displayed over the phone menu, which shouldn't happen.
+//The phone menu should be over everything.
+var removals = document.querySelectorAll('.hide-from-phone-menu');
+
+console.log(removals);
+
+
+
 function burgerClick(){
 
     var burgerMenu = document.querySelector('.nav-bar-burger');
@@ -15,15 +27,22 @@ function burgerClick(){
 
     if(openedMenu == null) //if it doesn't, then the menu is currently closed 
     {
+        //Hide all elements that use intersection observers because they 
+        //disregard z-index and are displayed over the phone menu.
+        for(var i = 0; i < removals.length; i++)
+        {
+            removals[i].style.visibility = "hidden";
+        }
+
         burgerMenu.classList.add('burger-menu-active'); //add the class to burger menu
                                                     //signifying that the burger menu should now be open.
         //Make an X with the burger spans
         spans[0].style.transformOrigin = "0% 0%";
-        spans[1].style.transformOrigin = "0% 100%";
-        spans[2].style.opacity = "0";
+        spans[2].style.transformOrigin = "0% 100%";
+        spans[1].style.opacity = "0";
                                                 
         spans[0].style.transform = "rotate(45deg) translate(-2px, -1px)";
-        spans[1].style.transform = "rotate(-45deg) translate(-10px, 10px)";
+        spans[2].style.transform = "rotate(-45deg) translate(-4px, 3px)";
 
         //make sure the rest of the page is empty so nothing pops-up over the menu.
        // content.style.position = "absolute";
@@ -32,31 +51,37 @@ function burgerClick(){
         //display the menu links
         burgerMenuLinks.classList.add("burger-menu-open");
 
-
-        //each link fades-in a few hundred milliseconds after the last link.
-        var linkFadeIn = 2000;
-
         for(var i = 0; i < burgerMenuLinks_li.length; i++)
         {
-          //  burgerMenuLinks_li[i].style.transition = linkFadeIn + "ms ease-in";
-
-            burgerMenuLinks_li[i].classList.add('burger-menu-link_fade-in');
-
-           // linkFadeIn = linkFadeIn + 600;
+            burgerMenuLinks_li[i].style.visibility = "visible";
         }
     }
     else
     {   //if it does exist, then the menu is currently open
+        
+        //Display all elements that use intersection observers because they 
+        //disregard z-index and are displayed over the phone menu. Since phone meny is closed,
+        //these elements can become visible again.
+        for(var i = 0; i < removals.length; i++)
+        {
+            removals[i].style.visibility = "visible";
+        }
+
         burgerMenu.classList.remove('burger-menu-active'); //remove the class from burger menu
-                                                        //signifying that the menu should now be closed.
+        //signifying that the menu should now be closed.
 
         //Make an burger with the burger spans
         spans[0].style.transformOrigin = "0% 0%";
-        spans[1].style.transformOrigin = "0% 0%";
-        spans[2].style.opacity = "1";
+        spans[2].style.transformOrigin = "0% 0%";
+        spans[1].style.opacity = "1";
                                                         
         spans[0].style.transform = "rotate(0deg) translate(0px)";
-        spans[1].style.transform = "rotate(0deg) translate(0px)";
+        spans[2].style.transform = "rotate(0deg) translate(0px)";
+
+        for(var i = 0; i < burgerMenuLinks_li.length; i++)
+        {
+            burgerMenuLinks_li[i].style.visibility = "hidden";
+        }
 
         //make sure the rest of the page is displayed again
         //content.style.position = "relative";
@@ -64,7 +89,9 @@ function burgerClick(){
 
         //Hide the menu links
         burgerMenuLinks.classList.remove("burger-menu-open");
-        burgerMenuLinks.classList.add("burger-menu-close");
+
+
+       
     }
 
 }
