4691871d4afe32b558d7e5fe9ef68b1a66b57a38
charlesQuigley <37884135+charlesQuigley@users.noreply.github.com>
2022-02-18 10:39:33 -0600

    Testing different way to do Parallax 8 - Smoothing Paralax Scroll for Mobile

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ window.addEventListener('scroll', function() {
     let scrollPosition = window.scrollY;
     console.log(scrollPosition);
 
-    parallax.style.transform = 'translateY(' + scrollPosition * 0.6 +'px)';
+    parallax.style.transform = 'translateY(' + scrollPosition * 0.4 +'px)';
 
 
 });
