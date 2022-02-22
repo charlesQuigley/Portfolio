5a6d1e0fb9ea50ea5b5a89f6081bb99897e1ecee
charlesQuigley <37884135+charlesQuigley@users.noreply.github.com>
2022-02-21 19:01:09 -0600

    Projects Page Responsive Design: Tablets (Portrait and Landscape) and Phones (Portrait) Completed

--- projects.js
+++ projects.js
@@ -196,8 +196,31 @@ var modalImg = document.getElementById("image");
 
 for(var i = 0; i < slidesOnClick.length; i++)
 {
+
     slidesOnClick[i].onclick = function(){
+        var isFullScreenImg = false;
+
         modal.style.display = "block";
+
+        modalImg.classList.remove('fullPageModalImg');
+        
+        //If image is a full-screen image (an image of an ENTIRE web page), 
+        //then it will have the class 'fullPageImage'.
+        //If it has this class, then resize the image so that it fits nicely within the screen. 
+        for(var j = 0; j < this.classList.length; j++)
+        {
+            if(this.classList[j] == 'fullPageImage')
+            {
+                isFullScreenImg = true;
+                break;
+            }
+        }
+
+        if(isFullScreenImg == true)
+        {
+            modalImg.classList.add('fullPageModalImg');
+        }
+
         modalImg.src = this.src;
     }
 }
